9b0b2bd9d2823bd528e5a53af9e20ee04b68a4d8
Kartikey <kartikeydubey.987@webkul.in>
2024-12-19 16:58:56 +0530

    update

--- tests/shop/customer.spec.ts
+++ tests/shop/customer.spec.ts
@@ -75,8 +75,23 @@ test('Delete Profile', async ({page}) => {
   await page.getByPlaceholder('Enter your password').click();
   await page.getByPlaceholder('Enter your password').fill('testUser@1234');
   await page.getByRole('button', { name: 'Delete' }).click();
+  
   try {
-    await page.waitForNavigation({ timeout: 5000 });
+    await page.getByRole('link', { name: 'Create your account' }).click();
+    await page.getByPlaceholder('First Name').click();
+    await page.getByPlaceholder('First Name').fill('testUser');
+    await page.getByPlaceholder('Last Name').click();
+    await page.getByPlaceholder('Last Name').fill('Demo');
+    await page.getByPlaceholder('email@example.com').click();
+    await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
+    await page.getByPlaceholder('Password', { exact: true }).click();
+    await page.getByPlaceholder('Password', { exact: true }).fill('testUser@123');
+    await page.getByPlaceholder('Confirm Password').click();
+    await page.getByPlaceholder('Confirm Password').fill('testUser@123');
+    await page.locator('#main form div').filter({ hasText: 'Subscribe to newsletter' }).locator('label').first().click();
+    await page.getByRole('button', { name: 'Register' }).click();
+    await page.getByText('Account created successfully.').first().click();
+
     console.log(page.url());
   } catch(e) {
     console.log(page.url());
@@ -86,20 +101,7 @@ test('Delete Profile', async ({page}) => {
 test('Add Address', async ({page}) => {
   await page.goto(`${config.baseUrl}`);
   await page.getByLabel('Profile').click();
-  await page.getByRole('link', { name: 'Sign Up' }).click();
-  await page.getByPlaceholder('First Name').click();
-  await page.getByPlaceholder('First Name').fill('testUser');
-  await page.getByPlaceholder('Last Name').click();
-  await page.getByPlaceholder('Last Name').fill('Demo');
-  await page.getByPlaceholder('email@example.com').click();
-  await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
-  await page.getByPlaceholder('Password', { exact: true }).click();
-  await page.getByPlaceholder('Password', { exact: true }).fill('testUser@123');
-  await page.getByPlaceholder('Confirm Password').click();
-  await page.getByPlaceholder('Confirm Password').fill('testUser@123');
-  await page.locator('#main form div').filter({ hasText: 'Subscribe to newsletter' }).locator('label').first().click();
-  await page.getByRole('button', { name: 'Register' }).click();
-  await page.getByText('Account created successfully.').first().click();
+  await page.getByRole('link', { name: 'Sign In' }).click();
   await page.getByPlaceholder('email@example.com').click();
   await page.getByPlaceholder('email@example.com').fill('testUser@gmail.com');
   await page.getByPlaceholder('Password').click();
